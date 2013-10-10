7ea4d48a0a3bdb024c50ea7086f102af14c788b1
Nikolay Kondikov <nikolay.kondikov@sirma.bg>
2013-10-10 12:15:55 +0300

    Search page initial files You have to update main.php manually by getting changes from main.install.php

--- www/protected/controllers/SiteController.php
+++ www/protected/controllers/SiteController.php
@@ -2,98 +2,103 @@
 
 class SiteController extends Controller
 {
-	public function filters() {
-    return array( // add blocked IP filter here
-      'Installed',
-      'IPBlock',
-    );
-  }
-	
-	/**
-	 * Declares class-based actions.
-	 */
-	public function actions()
-	{
-		return array(
-			// captcha action renders the CAPTCHA image displayed on the contact page
-			'captcha'=>array(
-				'class'=>'CCaptchaAction',
-				'backColor'=>0xFFFFFF,
-			),
-			// page action renders "static" pages stored under 'protected/views/site/pages'
-			// They can be accessed via: index.php?r=site/page&view=FileName
-			'page'=>array(
-				'class'=>'CViewAction',
-			),
-		);
-	}
-
-	/**
-	 * This action displays the arcade it's template can be found under views/site/index.php
-	 */
-	public function actionIndex()
-	{
-		MGHelper::setFrontendTheme();
-
-    $this->layout = '//layouts/arcade';
-    
-    $games = GamesModule::listActiveGames();
-    
-		// renders the view file 'protected/views/site/index.php'
-		// using the default layout 'protected/views/layouts/main.php'
-		$this->render('index', array(
-		  'games' => $games,
-    ));
-	}
-
-	/**
-	 * This is the action to handle external exceptions.
-	 */
-	public function actionError()
-	{
-	  MGHelper::setFrontendTheme();
-    
-    
-    if($error=Yii::app()->errorHandler->error) {
-    	if(Yii::app()->request->isAjaxRequest) {
-    		 echo $error['message'];
-    	}else {
-         $this->render('error', $error);
-      }
+    public function filters()
+    {
+        return array( // add blocked IP filter here
+            'Installed',
+            'IPBlock',
+        );
     }
-	}
 
-	/**
-	 * Displays the contact page
-	 */
-	public function actionContact()
-	{
-		MGHelper::setFrontendTheme();
+    /**
+     * Declares class-based actions.
+     */
+    public function actions()
+    {
+        return array(
+            // captcha action renders the CAPTCHA image displayed on the contact page
+            'captcha' => array(
+                'class' => 'CCaptchaAction',
+                'backColor' => 0xFFFFFF,
+            ),
+            // page action renders "static" pages stored under 'protected/views/site/pages'
+            // They can be accessed via: index.php?r=site/page&view=FileName
+            'page' => array(
+                'class' => 'CViewAction',
+            ),
+        );
+    }
 
-		$model=new ContactForm;
-		if(isset($_POST['ContactForm'])) {
-			$model->attributes=$_POST['ContactForm'];
-			if($model->validate())
-			{
-				$headers="From: {$model->email}\r\nReply-To: {$model->email}";
-				mail(Yii::app()->fbvStorage->get("settings.app_email"),$model->subject,$model->body,$headers);
-        Flash::add('success', Yii::t('app', 'Thank you for contacting us. We will respond to you as soon as possible.'));
-				$this->refresh();
-			}
-		}
-		$this->render('contact',array('model'=>$model));
-	}
+    /**
+     * This action displays the arcade it's template can be found under views/site/index.php
+     */
+    public function actionIndex()
+    {
+        $this->redirect(array('/search'));
+    }
 
-	/**
-	 * Displays the Credits page.
-	 */
- 	public function actionCredits()
-	{
-		MGHelper::setFrontendTheme();
-    
-		//$this->render('contact',array('model'=>$model));
-		$this->render('credits');
-	}
+    public function actionArcade()
+    {
+        MGHelper::setFrontendTheme();
+
+        $this->layout = '//layouts/arcade';
+
+        $games = GamesModule::listActiveGames();
+
+        // renders the view file 'protected/views/site/index.php'
+        // using the default layout 'protected/views/layouts/main.php'
+        $this->render('index', array(
+            'games' => $games,
+        ));
+    }
+
+    /**
+     * This is the action to handle external exceptions.
+     */
+    public function actionError()
+    {
+        MGHelper::setFrontendTheme();
+
+
+        if ($error = Yii::app()->errorHandler->error) {
+            if (Yii::app()->request->isAjaxRequest) {
+                echo $error['message'];
+            } else {
+                $this->render('error', $error);
+            }
+        }
+    }
+
+    /**
+     * Displays the contact page
+     */
+    public function actionContact()
+    {
+        MGHelper::setFrontendTheme();
+
+        $model = new ContactForm;
+        if (isset($_POST['ContactForm'])) {
+            $model->attributes = $_POST['ContactForm'];
+            if ($model->validate()) {
+                $headers = "From: {$model->email}\r\nReply-To: {$model->email}";
+                mail(Yii::app()->fbvStorage->get("settings.app_email"), $model->subject, $model->body, $headers);
+                Flash::add('success', Yii::t('app', 'Thank you for contacting us. We will respond to you as soon as possible.'));
+                $this->refresh();
+            }
+        }
+        $this->render('contact', array('model' => $model));
+    }
+
+    /**
+     * Displays the Credits page.
+     */
+    public function actionCredits()
+    {
+        MGHelper::setFrontendTheme();
+
+        //$this->render('contact',array('model'=>$model));
+        $this->render('credits');
+    }
 
     //action only for the login from third-party authentication providers, such as Google, Facebook etc. Not for direct login using username/password
 
@@ -102,16 +107,15 @@ class SiteController extends Controller
      * @param $provider
      * @param null $backUrl
      */
-    public function actionLogin($provider,$backUrl = null)
+    public function actionLogin($provider, $backUrl = null)
     {
-        if($backUrl == null)
-        {
-            $backUrl = Yii::app()->getRequest()->getHostInfo().Yii::app()->createUrl('/'); //  [Default behaviour => click]
+        if ($backUrl == null) {
+            $backUrl = Yii::app()->getRequest()->getHostInfo() . Yii::app()->createUrl('/'); //  [Default behaviour => click]
         }
 
-        try{
-            MGHelper::SocialLogin($provider,$backUrl);
-        }catch (CHttpException $e){
+        try {
+            MGHelper::SocialLogin($provider, $backUrl);
+        } catch (CHttpException $e) {
 
         }
     }
@@ -124,13 +128,13 @@ class SiteController extends Controller
         require_once $path . '/hybridauth-' . HybridAuthIdentity::VERSION . '/index.php';
     }
 
-	/**
-	 * Displays the About page.
-	 */
- 	public function actionAbout()
-	{
-		MGHelper::setFrontendTheme();
-		$this->render('about');
-	}
+    /**
+     * Displays the About page.
+     */
+    public function actionAbout()
+    {
+        MGHelper::setFrontendTheme();
+        $this->render('about');
+    }
 
 }
