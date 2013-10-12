f22735323d430763483e3da18619649a57bb3b52
petko.kostov <petko.kostov@sirma.bg>
2013-10-12 16:05:50 +0300

    Fix paginating.

--- www/protected/modules/search/views/default/index.php
+++ www/protected/modules/search/views/default/index.php
@@ -39,8 +39,11 @@ $this->widget('zii.widgets.CListView', array(
     ),
     'enablePagination'=>true,
     'template'=>"{summary}\n{sorter}\n{pager}\n{items}\n{sorter}\n{pager}", //pager on top
-    'summaryText'=>"Your search " . "<div id=\"searchedValue\"> </div>" ." returned " . totalItemsFind($model->search(true)) . " results",
+   'summaryText'=>" ",
+
 ));
 echo CHtml::endForm();
+echo  'Your search ' . "<div id=\"searchedValue\"> </div>" . 'returned ' . totalItemsFind($model->search(true)). ' results';
+
 
 ?>
