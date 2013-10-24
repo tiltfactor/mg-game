1b84428eb9b767bcc9c99379341abd0d9b3d9586
Petko.Kostov <petko.kostov@sirma.bg>
2013-10-24 19:00:44 +0300

    Pyramid front-end sceleton.

--- www/protected/modules/games/assets/pyramid/js/mg.game.pyramid.main.js
+++ www/protected/modules/games/assets/pyramid/js/mg.game.pyramid.main.js
@@ -114,78 +114,78 @@ $( document ).ready(function() {
 });
 
 /*
-$( document ).ready(function() {
-    if (is_touch_device) {
-        $("body").addClass('touch_device');
-    }
+ $( document ).ready(function() {
+ if (is_touch_device) {
+ $("body").addClass('touch_device');
+ }
 
-    if (Modernizr.highres) {
-        $("body").addClass('retina');
-    }
+ if (Modernizr.highres) {
+ $("body").addClass('retina');
+ }
 
-    BrowserDetect.init();
-    $("body").addClass(BrowserDetect.browser);
-})
+ BrowserDetect.init();
+ $("body").addClass(BrowserDetect.browser);
+ })
 
-Modernizr.addTest('highres', function() {
+ Modernizr.addTest('highres', function() {
 
-    // for opera
-    var ratio = '2.99/2';
-    // for webkit
-    var num = '1.499';
-    var mqs = [
-        'only screen and (-o-min-device-pixel-ratio:' + ratio + ')',
-        'only screen and (min--moz-device-pixel-ratio:' + ratio + ')',
-        'only screen and (-webkit-min-device-pixel-ratio:' + num + ')',
-        'only screen and (min-device-pixel-ratio:' + num + ')'
-    ];
-    var isHighRes = false;
+ // for opera
+ var ratio = '2.99/2';
+ // for webkit
+ var num = '1.499';
+ var mqs = [
+ 'only screen and (-o-min-device-pixel-ratio:' + ratio + ')',
+ 'only screen and (min--moz-device-pixel-ratio:' + ratio + ')',
+ 'only screen and (-webkit-min-device-pixel-ratio:' + num + ')',
+ 'only screen and (min-device-pixel-ratio:' + num + ')'
+ ];
+ var isHighRes = false;
 
-    // loop through vendors, checking non-prefixed first
-    for (var i = mqs.length - 1; i >= 0; i--) {
-        isHighRes = Modernizr.mq( mqs[i] );
-        // if found one, return early
-        if ( isHighRes ) {
-            return isHighRes;
-        }
-    }
-    // not highres
-    return isHighRes;
+ // loop through vendors, checking non-prefixed first
+ for (var i = mqs.length - 1; i >= 0; i--) {
+ isHighRes = Modernizr.mq( mqs[i] );
+ // if found one, return early
+ if ( isHighRes ) {
+ return isHighRes;
+ }
+ }
+ // not highres
+ return isHighRes;
 
-});
+ });
 
-var BrowserDetect = {
-    init: function () {
-        this.browser = this.searchString(this.dataBrowser) || "Other";
-        this.version = this.searchVersion(navigator.userAgent) ||       this.searchVersion(navigator.appVersion) || "Unknown";
-    },
+ var BrowserDetect = {
+ init: function () {
+ this.browser = this.searchString(this.dataBrowser) || "Other";
+ this.version = this.searchVersion(navigator.userAgent) ||       this.searchVersion(navigator.appVersion) || "Unknown";
+ },
 
-    searchString: function (data) {
-        for (var i=0 ; i < data.length ; i++) {
-            var dataString = data[i].string;
-            this.versionSearchString = data[i].subString;
+ searchString: function (data) {
+ for (var i=0 ; i < data.length ; i++) {
+ var dataString = data[i].string;
+ this.versionSearchString = data[i].subString;
 
-            if (dataString.indexOf(data[i].subString) != -1) {
-                return data[i].identity;
-            }
-        }
-    },
+ if (dataString.indexOf(data[i].subString) != -1) {
+ return data[i].identity;
+ }
+ }
+ },
 
-    searchVersion: function (dataString) {
-        var index = dataString.indexOf(this.versionSearchString);
-        if (index == -1) return;
-        return parseFloat(dataString.substring(index+this.versionSearchString.length+1));
-    },
+ searchVersion: function (dataString) {
+ var index = dataString.indexOf(this.versionSearchString);
+ if (index == -1) return;
+ return parseFloat(dataString.substring(index+this.versionSearchString.length+1));
+ },
 
-    dataBrowser:
-        [
-            { string: navigator.userAgent, subString: "Chrome",  identity: "Chrome" },
-            { string: navigator.userAgent, subString: "MSIE",    identity: "IE" },
-            { string: navigator.userAgent, subString: "Firefox", identity: "FF" },
-            { string: navigator.userAgent, subString: "Safari",  identity: "Safari" },
-            { string: navigator.userAgent, subString: "Opera",   identity: "Opera" }
-        ]
+ dataBrowser:
+ [
+ { string: navigator.userAgent, subString: "Chrome",  identity: "Chrome" },
+ { string: navigator.userAgent, subString: "MSIE",    identity: "IE" },
+ { string: navigator.userAgent, subString: "Firefox", identity: "FF" },
+ { string: navigator.userAgent, subString: "Safari",  identity: "Safari" },
+ { string: navigator.userAgent, subString: "Opera",   identity: "Opera" }
+ ]
 
-};
+ };
 
-*/
+ */
