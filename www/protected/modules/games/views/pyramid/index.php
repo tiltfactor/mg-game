a36e4208a6fe43d0998a7a8c24c74c9a9f3f4d0f
Delchina Angelova <delchina@sirma.bg>
2013-10-28 16:01:29 +0200

    Pyramid tag Remove Register from menu navigation

--- www/protected/modules/games/views/pyramid/index.php
+++ www/protected/modules/games/views/pyramid/index.php
@@ -112,7 +112,7 @@
             <ul>
                 <li class="back_blue row"><div><a href="#" location="main_screen"><span>HOME</span></a></div></li>
                 <li class="back_blue row hidden" id="mmenuPlay"><div><a href="<?php echo Yii::app()->baseUrl; ?>/index.php/games/Pyramid/play/"><span>PLAY</span></a></div></li>
-                <li class="back_blue row touch" id="mmenuRegister"><div><a href="#" location="register"><span>REGISTER</span></a></div></li>
+                <!--<li class="back_blue row touch" id="mmenuRegister"><div><a href="#" location="register"><span>REGISTER</span></a></div></li>-->
                 <li class="back_blue row touch" id="mmenuLogin"><div><a  href="#" location="login"><span>LOGIN</span></a></div></li>
                 <li class="back_blue row"><div><a href="#" location="how_to"><span>HOW TO PLAY</span></a></div></li>
                 <li class="back_blue row"><div><a href="#" location="learn_more"><span>LEARN MORE</span></a></div></li>
