34381645f3502671ee022eacc2f1f259f7ec393b
sukie punjasthitkul <sukie@tiltfactor.org>
2013-12-16 20:44:08 -0500

    ONE UP - update Android code to open link in new browser window
    Previous version still opened in game app instead of a separate
    browser window, so update java to correct this.

--- mobile/android/oneup/src/com/panaton/mgoneup/MainActivity.java
+++ mobile/android/oneup/src/com/panaton/mgoneup/MainActivity.java
@@ -12,16 +12,16 @@ import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 
 public class MainActivity extends Activity {
-	
+
 	private class OneUpWebViewClient extends WebViewClient {
 	    @Override
 	    public boolean shouldOverrideUrlLoading(WebView view, String url) {
 	        if (url.equals("https://gameServerLocation")) {
-	            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-	            startActivity(intent);
-	            return true;
+	            return false;
 	        }
-	        return false;
+	        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+	            view.getContext().startActivity(intent);
+	            return true;
 	    }
 	}
 
@@ -31,13 +31,13 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		myWebView = (WebView) findViewById(R.id.pyramid_webview);
 		myWebView.loadUrl(getResources().getString(R.string.gameUrl));
 		WebSettings webSettings = myWebView.getSettings();
 		webSettings.setJavaScriptEnabled(true);
 		myWebView.setWebViewClient(new OneUpWebViewClient());
-		
+
 		final ProgressBar progressWebView = (ProgressBar) findViewById(R.id.progress_webview);
 		myWebView.setWebChromeClient(new WebChromeClient() {
 			@Override
@@ -48,16 +48,16 @@ public class MainActivity extends Activity {
 				if(progress < 100 && progressWebView.getVisibility() == ProgressBar.GONE){
 					progressWebView.setVisibility(ProgressBar.VISIBLE);
                 }
-				
+
                 if(progress == 100) {
                 	progressWebView.setVisibility(ProgressBar.GONE);
-                    
+
                 }
 			}
 		});
-		
+
 	}
-		
+
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 	    // Check if the key event was the Back button and if there's history
