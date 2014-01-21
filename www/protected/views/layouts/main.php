a811ec49e17271ea8eb2eee4b0f42edbe18aee4a
sukie punjasthitkul <sukie@tiltfactor.org>
2014-01-20 19:03:03 -0500

    Restrict Arcade access to non-player roles

--- www/protected/views/layouts/main.php
+++ www/protected/views/layouts/main.php
@@ -2,12 +2,11 @@
 <div id="header">
   <a id="page_title" class="ir" href="<?php echo MGHelper::bu("/"); ?>"><?php CHtml::encode(Yii::app()->fbvStorage->get("settings.app_name")); ?></a>
   <div id="mainmenu">
-  <?php 
-  $user = User::loadUser(Yii::app()->user->id);
+  <?php
   $this->widget('application.components.MGMenu',array(
     'items'=>array(
       array('label'=>'Search', 'url'=>array('/search')),
-      array('label'=>'Arcade', 'url'=>array('/site/arcade'), 'visible'=>( $user) ), // junjie guan: control the acrade here
+      array('label'=>'Arcade', 'url'=>array('/site/arcade'), 'visible'=>(Yii::app()->user->checkAccess(EDITOR) || Yii::app()->user->checkAccess(INSTITUTION))), // junjie guan: control the acrade here
       array('label'=>'Contact', 'url'=>array('/site/contact')),
       array('url'=>Yii::app()->getModule('user')->loginUrl, 'label'=>Yii::app()->getModule('user')->t("Login"), 'visible'=>Yii::app()->user->isGuest),
       array('url'=>Yii::app()->getModule('user')->registrationUrl, 'label'=>Yii::app()->getModule('user')->t("Register"), 'visible'=>Yii::app()->user->isGuest),
@@ -15,8 +14,8 @@
       array('url'=>Yii::app()->getModule('user')->profileUrl, 'label'=>Yii::app()->getModule('user')->t("Profile"), 'visible'=>!Yii::app()->user->isGuest),
       array('url'=>Yii::app()->getModule('user')->logoutUrl, 'label'=>Yii::app()->getModule('user')->t("Logout").' ('.Yii::app()->user->name.')', 'visible'=>!Yii::app()->user->isGuest)
     ),
-    
-  )); 
+
+  ));
   ?></div><!-- mainmenu -->
 
   <div id="usersonline">
@@ -34,7 +33,7 @@
 </div>
 
 <div class="container" id="page">
-  
+
   <?php if(isset($this->breadcrumbs)):?>
     <?php $this->widget('zii.widgets.CBreadcrumbs', array(
       'homeLink' =>CHtml::link(Yii::t('app', 'Arcade'), "/site/arcade"),
@@ -42,7 +41,7 @@
     )); ?><!-- breadcrumbs -->
   <?php endif?>
   <?php $this->widget('application.extensions.yii-flash.Flash', array(
-    'keys'=>array('success', 'warning','error'), 
+    'keys'=>array('success', 'warning','error'),
     'htmlOptions'=>array('class'=>'flash'),
   )); ?><!-- flashes -->
 
