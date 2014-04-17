ae807bec63ffd71928e19d456724b1d4d378ccab
redhatkaty <you@example.com>
2014-04-17 17:57:53 -0400

    change login, let it redirct to a game page after login

--- www/protected/modules/user/controllers/LoginController.php
+++ www/protected/modules/user/controllers/LoginController.php
@@ -19,17 +19,32 @@ class LoginController extends Controller
     
 		if (Yii::app()->user->isGuest) {
 			$model=new UserLogin;
-			// collect user input data
+
+            // if called without specific returnUrl, set it to the previous page
+            if (!isset($_POST['UserLogin']) && Yii::app()->user->returnUrl === Yii::app()->request->scriptUrl)
+            {
+                if (isset($_SERVER['HTTP_REFERER']))
+                {
+                    Yii::app()->user->setReturnUrl($_SERVER['HTTP_REFERER']);
+                }
+            }
+
+            // collect user input data
 			if(isset($_POST['UserLogin']))
 			{
 				$model->attributes=$_POST['UserLogin'];
 				// validate user input and redirect to previous page if valid
 				if($model->validate()) {
-					$model->setLastVisit();
-					if (strpos(Yii::app()->user->returnUrl,'/index.php')!==false)
-						$this->redirect(Yii::app()->controller->module->returnUrl);
-					else
+                    $model->setLastVisit();
+                    //find the url that have /games in it, only return if come from a game page
+					if (strpos(Yii::app()->user->returnUrl,'/index.php/games')!==false)
 						$this->redirect(Yii::app()->user->returnUrl);
+					else
+						$this->redirect(Yii::app()->controller->module->returnUrl);
+//                    if (strpos(Yii::app()->user->returnUrl,'/index.php')!==false)
+//                        $this->redirect(Yii::app()->controller->module->returnUrl);
+//                    else
+//                        $this->redirect(Yii::app()->user->returnUrl);
 				}
 			}
 			// display the login form
