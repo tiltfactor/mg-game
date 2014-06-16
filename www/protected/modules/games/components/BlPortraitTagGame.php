7cc0a57cd008cbe5e2c1d135c0c2b8d830c72418
Gareth Solbeck <gsolbeck@gmail.com>
2014-06-16 15:40:31 -0400

    Apply Zentag querying to Zentag reskins, and update Zentag media limit to 300 This commit applies the new ZenTag querying to the various derivatives of ZenTag (Ships Tag, Book Tag, and Portrait Tag). In addition, it updates the media limit used by the query from 20 (used in testing) to 300.

--- www/protected/modules/games/components/BlPortraitTagGame.php
+++ www/protected/modules/games/components/BlPortraitTagGame.php
@@ -102,7 +102,7 @@ class BlPortraitTagGame extends MGGame implements MGGameInterface
             $used_medias = array();
 
             // get a one medias that is active for the game
-            $medias = $this->getMedias($collections, $game, $game_model);
+            $medias = $this->getMediasforZentag($collections, $game, $game_model);
 
 
             if ($medias && count($medias) > 0) {
